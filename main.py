85e81f74562bcc24f3e93ad01dc0c3901deeeb8c
Burak Yildiz <145865827+burakkyildiz@users.noreply.github.com>
2025-04-22 16:01:54 +0300

    create project

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from pdf2docx import Converter
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+#enter pdf path
+pdf_file = r"example.pdf"
 
+#enter output docx path
+docx_file = r"example.docx"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+cv = Converter(pdf_file)
+cv.convert(docx_file)
+cv.close()
